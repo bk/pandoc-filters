582657f2b9aaf11a1f8626f5f81111a533e669f8
Baldur Kristinsson <bk@mbl.is>
2022-07-23 17:07:38 +0000

    Improved roff ms images support

--- image-ms.lua
+++ image-ms.lua
@@ -1,98 +1,152 @@
-local text = require 'text'
+-- Rudimentary image support for pandoc roff ms export.
+--
+-- A rewritten and simplified version of the filter by Bernard Fisseni (teoric):
+-- https://github.com/teoric/pandoc-filters/blob/master/image-ms.lua
+--
+-- Limitations:
+--   * Works only with local images.
+--   * Assumes that each image takes up a "paragraph".
+--   * Image directories must be writable or already contain PDF images.
+--   * PDF conversion requires ImageMagick (the "convert" command).
+--   * Automatic size determination requires pdfinfo (part of Poppler).
+
 local utils = require 'pandoc.utils'
 local system = require 'pandoc.system'
 local path = require 'pandoc.path'
 
-function endswith(String, End)
-  return End == '' or string.sub(String, - string.len(End)) == End
+-- in points -- about 19.05 cm
+local max_width = 540
+-- Since all images take up their own paragraph, very small ones
+-- do not make much sense. Let's say about 1 cm min width.
+local min_width = 28
+
+function endswith(str, ending)
+  return ending == '' or str:sub(-#ending) == ending
 end
 
-function file_exists(name)
+function file_exists(path)
   -- file exists if it is readable
-  local f = io.open(name, "r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
+  local file = io.open(path, "rb")
+  if file then file:close() end
+  return file ~= nil
+end
+
+function get_img_size(given_w, given_h, src)
+  if given_w ~= nil then
+    given_w = img_dim(given_w, min_width, max_width)
+    if given_h ~= nil then
+      given_h = img_dim(given_h, 1, max_width * 1.4)
+    end
+    return given_w, given_h
+  end
+  -- Get intrinsic size from PDF file
+  size = pandoc.pipe('pdfinfo', {src}, "")
+  local w, h
+  _, _, w, h = size:find("Page size:%s+([%d%.]+)%s+x%s+([%d%.]+)")
+  if w then
+    return img_dim(w .. 'pt', min_width, max_width), img_dim(h .. 'pt', 1, max_width * 1.4)
   end
 end
 
-function convert_measurements(size, check_percent)
-  -- convert and guess HTML/LaTeX units to [gt]roff units
-  if size ~= nil then
-    -- print("size before: " .. size)
-    size = string.gsub(size, "[;:,]$", "")
-    -- Assumes 72 dpi and an approximate text width of 5.9in == 425p
-    -- This is not appropriate for height values.
-    if check_percent and endswith(size, '%') then
-      size = string.gsub(size, "[^%.%d]", "")
-      size = tonumber(size) * 0.01 * 425.0
-      size = tostring(size) .. 'p'
-    end
-    size = string.gsub(size, "pt$", "p") -- pt called p
-    size = string.gsub(size, "px$", "p") -- assuming 72 dpi, 1px is 1p
-    size = string.gsub(size, "em$", "m") -- em called m
-    size = string.gsub(size, "ex$", "n") -- ex called n
-    if (string.match(size, "^[0-9.]+cm") or
-      string.match(size, "^[0-9.]+p") or
-      string.match(size, "^[0-9.]+P") or
-      string.match(size, "^[0-9.]+in")) then
-      -- print("size after: " .. size)
-      return size
+function img_dim(x, min, max)
+  x = to_points(x)
+  if x ~= nil then
+    if x < min then
+      return tostring(min) .. 'p'
+    elseif x > max then
+      return tostring(max) .. 'p'
+    else
+      return tostring(x) .. 'p'
     end
   end
-  return nil
 end
+
+function to_points(val)
+  -- Convert HTML/CSS measurements to points (given certain assumptions).
+  -- Note that '%'/'vw' are only appropriate for width. Using them for height
+  -- will often yield inappropriate results.
+  -- First, remove trailing separators.
+  val = val:gsub('[;:,]$', '')
+  -- Note that px assumes 300dpi (not 96dpi), and that '%'/'vw' take the content
+  -- width (excluding) margins) to be about 425 points.
+  local known_units = {
+    pt=1.0, em=12.0, rem=12.0, en=6.0, ex=6.0, ch=6.0, mm=2.83, cm=28.3,
+    ["in"]=71.882, pc=12.0, px=0.24, vw=425, ["%"]=4.25, }
+  local unit
+  local val_num = tonumber(val)
+  if val_num ~= nil then
+    unit = 'px'
+  else
+    val_num, unit = val:match('([%d%.]+)([^%d%.]+)')
+  end
+  if val_num ~= nil and known_units[unit] then
+    return tonumber(val_num) * known_units[unit]
+  end
+end
+
+function get_absolute_path(needle)
+  -- Referencing the image by absolute path is necessary if the .ms file
+  -- is placed in a different directory than the markdown file or if it
+  -- depends on images in '--resource-path' directories.
+  try_dirs = {}
+  for _,v in ipairs(PANDOC_STATE.input_files) do
+    table.insert(try_dirs, path.directory(v))
+  end
+  if PANDOC_STATE.source_url ~= nil then
+    table.insert(try_dirs, PANDOC_STATE.source_url)
+  end
+  table.insert(try_dirs, system.get_working_directory())
+  for _, v in ipairs(PANDOC_STATE.resource_path) do
+    table.insert(try_dirs, v)
+  end
+  for k,v in ipairs(try_dirs) do
+    if file_exists(path.join({v, needle})) then
+      return v
+    end
+  end
+  return '.'
+end
+
 
 return {
   {
     Image = function (im)
       -- cf. https://github.com/jgm/pandoc/issues/4475
       if FORMAT == "ms" then
-        -- Assumes centering. Other options: -L, -R
-        pat = '.PDFPIC -C'
+        -- Assumes centering. Other options (-L, -R) are less appropriate
+        -- to an image taking up its own paragraph. Floats and inlines are
+        -- beyond our scope but are handled by teoric's filter mentioned above.
+        ret = '.PDFPIC -C'
         local im_src_old = im.src
-        local im_src_new = string.gsub(im.src, "%.[^.]+$", ".pdf")
-        if not endswith(text.lower(im.src), ".pdf") then
-          im.src = path.join({
-              system.get_working_directory(),
-              im_src_new})
+        if not endswith(im.src:lower(), ".pdf") then
+          local im_src_new = string.gsub(im.src, "%.[^.]+$", ".pdf")
+          if im_src_new:sub(1,1,'/') == '/' then
+            im.src = im_src_new
+          else
+            leading_path = get_absolute_path(im_src_old)
+            im.src = path.join({leading_path, im_src_new})
+            im_src_old = path.join({leading_path, im_src_old})
+          end
         end
         if not file_exists(im.src) and file_exists(im_src_old) then
           -- if this fails, check policies in /etc/ImageMagick-[67]/policy.xml
           -- whether they forbid conversion of EPS, PDF etc.
           pandoc.pipe("convert", {im_src_old, im.src}, "")
         end
-        pat = pat .. string.format(' "%s"', im.src)
-        if im.attributes ~= nil then
-          local no_explicit_width = true
-          if im.attributes.width ~= nil then
-            im.attributes.width = convert_measurements(im.attributes.width, true)
-            no_explicit_width = false
-          end
-          if im.attributes.height ~= nil then
-            im.attributes.height = convert_measurements(im.attributes.height, false)
-          end
-          size = pandoc.pipe('pdfinfo', {im.src}, "")
-          local w
-          local h
-          _, _, w, h = string.find(size, "Page size:%s+([%d.]+)%s+x%s+([%d.]+)")
-          local height = im.attributes.height or (no_explicit_width and h and (h .. "p")) or nil
-          local width = im.attributes.width or w and (w .. "p") or nil
-          if width ~= nil then
-            pat = pat .. string.format(' %s', width)
-            -- height only matters if width was given
-            if height ~= nil then
-              pat = pat .. string.format(' %s', height)
-            end
+        ret = ret .. string.format(' "%s"', im.src)
+        local width, height = get_img_size(im.attributes.width, im.attributes.height, im.src)
+        if width ~= nil then
+          ret = ret .. string.format(' %s', width)
+          -- height only matters if width was given
+          if height ~= nil then
+            ret = ret .. string.format(' %s', height)
           end
         end
         cap = utils.stringify(im.caption)
         if cap ~= nil and cap ~= '' then
-          pat = pat .. '\n.CD\n\\s-2\\f[I]' .. cap .. '\\f[R]\\s0\n.DE\n'
+          ret = ret .. '\n.CD\n\\s-2\\f[I]' .. cap .. '\\f[R]\\s0\n.DE\n'
         end
-        return pandoc.RawInline("ms", pat)
+        return pandoc.RawInline("ms", ret)
       end
     end,
   }
