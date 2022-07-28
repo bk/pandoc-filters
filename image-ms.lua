92216754c850c389e144cbd4755d7c3d33de10b2
Baldur Kristinsson <bk@mbl.is>
2022-07-28 07:42:52 +0000

    Handle inline images - after a fashion

--- image-ms.lua
+++ image-ms.lua
@@ -1,11 +1,17 @@
 -- Rudimentary image support for pandoc roff ms export.
+--   See also cf. https://github.com/jgm/pandoc/issues/4475
 --
 -- A rewritten and simplified version of the filter by Bernard Fisseni (teoric):
 -- https://github.com/teoric/pandoc-filters/blob/master/image-ms.lua
 --
 -- Limitations:
 --   * Works only with local images.
---   * Assumes that each image takes up a "paragraph".
+--   * Primarily intended for 'figures', i.e. images with caption, appearing
+--     by themselves in a paragraph.
+--   * Other images will appear also (without caption) but are not really
+--     inline; they will break the text and appear on a line by themselves.
+--   * Floats and inlines are thus beyond our scope but are handled by teoric's
+--     filter mentioned above.
 --   * Image directories must be writable or already contain PDF images.
 --   * PDF conversion requires ImageMagick (the "convert" command).
 --   * Automatic size determination requires pdfinfo (part of Poppler).
@@ -16,7 +22,7 @@ local path = require 'pandoc.path'
 
 -- in points -- about 19.05 cm
 local max_width = 540
--- Since all images take up their own paragraph, very small ones
+-- Since all images take up their own "paragraph", very small ones
 -- do not make much sense. Let's say about 1 cm min width.
 local min_width = 28
 
@@ -107,46 +113,74 @@ function get_absolute_path(needle)
   return '.'
 end
 
+function solo_image (im)
+  -- Centered image with italic caption in smaller font.
+  ret = '.PDFPIC -C'
+  local width, height
+  im, width, height = convert_and_get_size(im)
+  ret = ret .. string.format(' "%s"', im.src)
+  if width ~= nil then
+    ret = ret .. string.format(' %s', width)
+    -- height only matters if width was given
+    if height ~= nil then
+      ret = ret .. string.format(' %s', height)
+    end
+  end
+  cap = utils.stringify(im.caption)
+  if cap ~= nil and cap ~= '' then
+    ret = ret .. '\n.CD\n\\s-2\\f[I]' .. cap .. '\\f[R]\\s0\n.DE\n'
+  end
+  return pandoc.RawInline("ms", ret)
+end
+
+function inline_image (im)
+  local width, height
+  im, width, height = convert_and_get_size(im)
+  ret = "\n.PDFPIC -I 1 " .. string.format('"%s"', im.src)
+  if width ~= nil then
+    ret = ret .. string.format(' %s', width)
+    -- height only matters if width was given
+    if height ~= nil then
+      ret = ret .. string.format(' %s', height)
+    end
+  end
+  ret = ret .. '\n'
+  return pandoc.RawInline("ms", ret)
+end
+
+function convert_and_get_size(im)
+  local im_src_old = im.src
+  if not endswith(im.src:lower(), ".pdf") then
+    local im_src_new = string.gsub(im.src, "%.[^.]+$", ".pdf")
+    if im_src_new:sub(1,1,'/') == '/' then
+      im.src = im_src_new
+    else
+      leading_path = get_absolute_path(im_src_old)
+      im.src = path.join({leading_path, im_src_new})
+      im_src_old = path.join({leading_path, im_src_old})
+    end
+  end
+  if not file_exists(im.src) and file_exists(im_src_old) then
+    -- if this fails, check policies in /etc/ImageMagick-[67]/policy.xml
+    -- whether they forbid conversion of EPS, PDF etc.
+    pandoc.pipe("convert", {im_src_old, im.src}, "")
+  end
+  local width, height = get_img_size(im.attributes.width, im.attributes.height, im.src)
+  return im, width, height
+end
 
 return {
   {
-    Image = function (im)
-      -- cf. https://github.com/jgm/pandoc/issues/4475
-      if FORMAT == "ms" then
-        -- Assumes centering. Other options (-L, -R) are less appropriate
-        -- to an image taking up its own paragraph. Floats and inlines are
-        -- beyond our scope but are handled by teoric's filter mentioned above.
-        ret = '.PDFPIC -C'
-        local im_src_old = im.src
-        if not endswith(im.src:lower(), ".pdf") then
-          local im_src_new = string.gsub(im.src, "%.[^.]+$", ".pdf")
-          if im_src_new:sub(1,1,'/') == '/' then
-            im.src = im_src_new
-          else
-            leading_path = get_absolute_path(im_src_old)
-            im.src = path.join({leading_path, im_src_new})
-            im_src_old = path.join({leading_path, im_src_old})
-          end
-        end
-        if not file_exists(im.src) and file_exists(im_src_old) then
-          -- if this fails, check policies in /etc/ImageMagick-[67]/policy.xml
-          -- whether they forbid conversion of EPS, PDF etc.
-          pandoc.pipe("convert", {im_src_old, im.src}, "")
-        end
-        ret = ret .. string.format(' "%s"', im.src)
-        local width, height = get_img_size(im.attributes.width, im.attributes.height, im.src)
-        if width ~= nil then
-          ret = ret .. string.format(' %s', width)
-          -- height only matters if width was given
-          if height ~= nil then
-            ret = ret .. string.format(' %s', height)
-          end
-        end
-        cap = utils.stringify(im.caption)
-        if cap ~= nil and cap ~= '' then
-          ret = ret .. '\n.CD\n\\s-2\\f[I]' .. cap .. '\\f[R]\\s0\n.DE\n'
-        end
-        return pandoc.RawInline("ms", ret)
+    Inlines = function(inl)
+      if FORMAT == "ms" and #inl==1 and inl[1].t == 'Image' then
+        return solo_image(inl[1])
+      end
+    end,
+  },
+  {
+    Image = function(im)
+      if FORMAT == 'ms' then
+        return inline_image(im)
       end
     end,
   }
