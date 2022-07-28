ef206b4a70d66fd8316c5e3ecc9e0d5c4e382f44
Baldur Kristinsson <bk@mbl.is>
2022-07-28 13:06:31 +0000

    Added additional filters -- pending rename

--- tasklist-ms.lua
+++ tasklist-ms.lua
@@ -2,9 +2,6 @@ local utils = require 'pandoc.utils'
 
 t_done = "☒"
 t_todo = "☐"
-t_fix = {}
-t_fix[t_done] = '\\f[FSerR]' .. t_done .. '\\f[R]'
-t_fix[t_todo] = '\\f[FSerR]' .. t_todo .. '\\f[R]'
 
 function is_taskitem(it)
   if not (it.t == 'Plain' and #(it.content) > 0) then
@@ -16,13 +13,14 @@ end
 
 function fix_taskitem(item)
   sym = utils.stringify(item.content[1])
-  item.content[1] = pandoc.RawInline("ms", t_fix[sym])
+  item.content[1] = pandoc.RawInline("ms", '\\f[FSerR]' .. sym .. '\\f[R]')
   return item
 end
 
 return {
   {
     BulletList = function(bl)
+      -- Fixes taskitem symbols (uses the FreeRoman font)
       if FORMAT == 'ms' then
         return bl:walk {
           Plain = function(el)
@@ -31,6 +29,31 @@ return {
             end
           end
         }
+      end
+    end,
+    Underline = function(ul)
+      -- Makes underline really underlined, not italic
+      -- (NOTE: '"' inside underline causes error)
+      if FORMAT == 'ms' then
+        table.insert(ul.content, 1, pandoc.RawInline("ms", '\n.UL "'))
+        table.insert(ul.content, pandoc.RawInline("ms", '"\n'))
+        return ul.content
+      end
+    end,
+    Header = function(h)
+      -- Changes font in headlines based on variables for the output.
+      if FORMAT == 'ms' then
+        vars = PANDOC_WRITER_OPTIONS.variables or {}
+        -- We need to know this so we can switch back to it
+        docfam = utils.stringify(vars['fontfamily'] or '')
+        hfam = utils.stringify(vars['heading-fontfam'] or '')
+        lvl = tonumber(utils.stringify(vars['heading-fontfam-max-level'] or '3'))
+        if #docfam > 0 and #hfam > 0 and h.level <= lvl then
+          -- Renders headings up to level lvl in this font
+          table.insert(h.content, 1, pandoc.RawInline("ms", ".fam " .. hfam .. "\n"))
+          table.insert(h.content, pandoc.RawInline("ms", "\n.fam " .. docfam .. "\n"))
+          return pandoc.Header(h.level, h.content, h.attr)
+        end
       end
     end,
   },
